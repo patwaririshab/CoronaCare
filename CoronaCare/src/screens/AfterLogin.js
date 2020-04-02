52924c7c15e6ef14899226bf9b71ccc57ab00158
DarkArcZ <jeromewongzh@u.nus.edu>
2020-04-02 20:44:07 +0800

    * Updated Permissions for Camera on AndroidManifest
    * Installed react-native-camera package and linked it
    
    * Registered CameraScreen Navigation on navigation.js
    
    * Added onPress function to the camera button
    
    * CameraScreen added with no functionality yet

--- CoronaCare/src/screens/AfterLogin.js
+++ CoronaCare/src/screens/AfterLogin.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
 import {View, TextInput, Text, Button, StyleSheet} from 'react-native';
 import Colors from 'react-native/Libraries/NewAppScreen';
-
+import { Navigation } from 'react-native-navigation';
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    changeToCameraScreen = () => {
+        Navigation.setRoot({
+            root: {
+                component: {
+                    name: "navigation.CoronaCare.CameraScreen"
+                }
+            }
+        })
+    }
+
     render() {
         return (
             
@@ -11,6 +25,7 @@ export default class Home extends Component {
                 <Button 
                 title = "Take Picture"
                 color = "black"
+                onPress = {this.changeToCameraScreen.bind(this)}
                 />
                 
                 <Button
