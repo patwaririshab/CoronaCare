418d23afcceb04b8dadf0688a365ca2e5b4694df
kwokjunn <kwok.zi.jun.2015@vjc.sg>
2020-04-04 08:32:32 +0800

    Beautify_Login_Page

--- CoronaCare/src/screens/AfterLogin.js
+++ CoronaCare/src/screens/AfterLogin.js
@@ -1,18 +1,27 @@
 import React, {Component} from 'react';
-import {View, TextInput, Text, Button, StyleSheet} from 'react-native';
-import Colors from 'react-native/Libraries/NewAppScreen';
+import {View, StyleSheet} from 'react-native';
+import {Button} from 'react-native-elements';
+import { Icon } from 'react-native-elements'
+
+
 
 
 export default class Home extends Component {
     render() {
         return (
             
-            <View>
+           
+                <View>
+                    
                 <Button 
-                title = "Take Picture"
-                color = "black"
-                />
+                small
+                title = "Capture Temperature"
+                rightIcon={{name: 'camera'}}
+                type = "outline"
+                style = {styles.buttonFirst}
                 
+                />
+
                 <Button
                 title = "View Records"
                 color = "black"
@@ -22,7 +31,3 @@ export default class Home extends Component {
     }
 }
 
-const styles = StyleSheet.create({
-
-   
-});
