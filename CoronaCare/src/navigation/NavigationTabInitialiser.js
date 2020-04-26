80f4dfeb98f61eed9ee87a5d7e33750cc37bdf02
Rishab Patwari <patwariri@gmail.com>
2020-04-26 13:40:43 +0800

    fix: topBar shows up in AfterLoginScreen.js

--- CoronaCare/src/navigation/NavigationTabInitialiser.js
+++ CoronaCare/src/navigation/NavigationTabInitialiser.js
@@ -15,6 +15,11 @@ const NavigationTabInitialiser = () => {
                     passProps: {
                       text: 'This is tab 3',
                     },
+                    options: {
+                      topBar: {
+                        visible: false
+                      }
+                    }
                   },
                 }],
                 options: {
