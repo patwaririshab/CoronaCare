c001fc35669ea999327846774140de033c4465d6
Rishab Patwari <patwariri@gmail.com>
2020-05-26 14:48:31 +0800

    chore: deleted references to CoronaCare

--- CoronaCare/ios/CareRecord/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeDelegate.h>
-#import <UIKit/UIKit.h>
-
-@import GoogleSignIn;
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate, GIDSignInDelegate>
-
-@property (nonatomic, strong) UIWindow *window;
-
-@end
--- CoronaCare/src/screens/WebViewScreen.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import {WebView} from 'react-native-webview';
-import {WebPortalUrl} from '../services/WebPortalLinks';
-
-export default class WebViewScreen extends Component {
-  render() {
-    return <WebView source={{uri: WebPortalUrl}} style={{marginTop: 20}} />;
-  }
-}
